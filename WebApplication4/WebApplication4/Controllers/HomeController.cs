0a858b1cf84d520fdeac94e7051360ae20c6258f
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-12-15 15:08:12 +0200

    firstCommit

---
+++ WebApplication4/WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    [RoleFilter]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [RoleFilter]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
