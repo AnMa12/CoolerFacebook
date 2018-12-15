93d57c9307d70f4a8da455027384487ab86243be
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-12-15 15:10:13 +0200

    add RoleFIlter for Roles

---
+++ WebApplication4/WebApplication4/ActionFilters/RoleFilter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+public class RoleFilter : ActionFilterAttribute
+{
+    public override void OnActionExecuted(ActionExecutedContext filterContext)
+    {
+        if (filterContext.Result is ViewResultBase result)
+        {
+            var user = filterContext.HttpContext.User;
+            if (user.IsInRole("Admin"))
+            {
+                result.ViewName = "About";
+            }
+            else if (user.IsInRole("Registered"))
+            {
+                result.ViewName = "Contact";
+            }
+            else if (user.IsInRole("Unregistered"))
+            {
+                result.ViewName = "Index";
+            }
+        }
+    }
+}
