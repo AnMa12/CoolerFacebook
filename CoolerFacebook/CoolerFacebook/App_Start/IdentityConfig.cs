1af456205cde7df394f8cedf25866d5b73e4dfe6
laviniamihalachi <34279768+laviniamihalachi@users.noreply.github.com>
2019-01-02 13:16:12 -0800

    roluri

--- CoolerFacebook/CoolerFacebook/App_Start/IdentityConfig.cs
+++ CoolerFacebook/CoolerFacebook/App_Start/IdentityConfig.cs
@@ -106,4 +106,18 @@ namespace CoolerFacebook
             return new ApplicationSignInManager(context.GetUserManager<ApplicationUserManager>(), context.Authentication);
         }
     }
+    public class ApplicationRoleManager : RoleManager<IdentityRole>
+    {
+        public ApplicationRoleManager(IRoleStore<IdentityRole, string> store) :
+       base(store)
+        {
+        }
+        public static ApplicationRoleManager
+       Create(IdentityFactoryOptions<ApplicationRoleManager> options, IOwinContext context)
+        {
+            var roleStore = new
+           RoleStore<IdentityRole>(context.Get<ApplicationDbContext>());
+            return new ApplicationRoleManager(roleStore);
+        }
+    }
 }
--- CoolerFacebook/CoolerFacebook/Startup.cs
+++ CoolerFacebook/CoolerFacebook/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using CoolerFacebook.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(CoolerFacebook.Startup))]
@@ -9,6 +12,44 @@ namespace CoolerFacebook
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createAdminUserAndApplicationRoles();
+        }
+        private void createAdminUserAndApplicationRoles()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new
+           RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new
+           UserStore<ApplicationUser>(context));
+            // Se adauga rolurile aplicatiei
+            if (!roleManager.RoleExists("Administrator"))
+            {
+                // Se adauga rolul de administrator
+                var role = new IdentityRole();
+                role.Name = "Administrator";
+                roleManager.Create(role);
+                // se adauga utilizatorul administrator
+                var user = new ApplicationUser();
+                user.UserName = "admin@admin.com";
+                user.Email = "admin@admin.com";
+                var adminCreated = UserManager.Create(user, "Administrator1!");
+                if (adminCreated.Succeeded)
+                {
+                    UserManager.AddToRole(user.Id, "Administrator");
+                }
+            }
+            if (!roleManager.RoleExists("User"))
+            {
+                var role = new IdentityRole();
+                role.Name = "User";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Guest"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Guest";
+                roleManager.Create(role);
+            }
         }
     }
 }
