4dc39d048a792ec626593cf4c4786be97b110b3b
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2019-01-03 02:42:57 +0200

    add icons

--- CoolerFacebook/CoolerFacebook/Controllers/ProfileController.cs
+++ CoolerFacebook/CoolerFacebook/Controllers/ProfileController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -14,4 +15,5 @@ namespace CoolerFacebook.Controllers
             return View();
         }
     }
+
 }
