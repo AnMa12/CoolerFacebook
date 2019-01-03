60f200be10030ed056ce9f57efc6bfab0429d797
laviniamihalachi <34279768+laviniamihalachi@users.noreply.github.com>
2019-01-02 16:37:59 -0800

    modele

---
+++ CoolerFacebook/CoolerFacebook/Models/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CoolerFacebook.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+        [Required]
+        public string Text { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+
+        public virtual Photo Photo { get; set; }
+        public virtual ApplicationUser Author { get; set; }
+    }
+}
---
+++ CoolerFacebook/CoolerFacebook/Models/Photo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CoolerFacebook.Models
+{
+    public class Photo
+    {
+        [Key]
+        public int PhotoId { get; set; }
+        [Required]
+        public string Path { get; set; }
+        public string Description { get; set; }
+
+        public virtual Album Album { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
