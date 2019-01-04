11db9a314de5ffb77c39739ff3d5a70a647c55f5
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2019-01-04 04:06:09 +0200

    add friend controller and view

---
+++ CoolerFacebook/CoolerFacebook/Controllers/FriendController.cs
@@ -0,0 +1,23 @@
+﻿using CoolerFacebook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoolerFacebook.Controllers
+{ 
+    public class FriendController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        
+        // GET: Friend
+        public ActionResult Index()
+        {
+            var friends = from friend in db.Friends
+                         select friend;
+            ViewBag.Friends = friends;
+            return View();
+        }
+    }
+}
