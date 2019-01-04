a2e33e8f75ffa2c529a88e0429d4d467ed1788dd
laviniamihalachi <34279768+laviniamihalachi@users.noreply.github.com>
2019-01-04 15:31:03 -0800

    Chat Models and partial view index

---
+++ CoolerFacebook/CoolerFacebook/Controllers/ChatController.cs
@@ -0,0 +1,30 @@
+﻿using CoolerFacebook.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoolerFacebook.Controllers
+{
+    public class ChatController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Chat
+        public ActionResult Index()
+        {
+            var currentUserId = User.Identity.GetUserId();
+            Profile currentProfile = db.Profiles.Where(i => i.User.Id == currentUserId).FirstOrDefault();
+
+            var friends = from friend in db.Friends
+                          where friend.Friend1.ProfileId == currentProfile.ProfileId
+                          select friend;
+            ViewBag.friends = friends;
+
+            
+            return View();
+        }
+    }
+}
---
+++ CoolerFacebook/CoolerFacebook/Models/Chat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CoolerFacebook.Models
+{
+    public class Chat
+    {
+        [Key]
+        public int ChatId { get; set; }
+        public virtual Profile Friend1 { get; set; }
+        public virtual Profile Friend2 { get; set; }
+
+        public virtual ICollection<Message> Messages { get; set;}
+    }
+}
