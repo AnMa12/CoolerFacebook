3bdd39d8ab9feeb853f4543631ec3230019c802f
laviniamihalachi <34279768+laviniamihalachi@users.noreply.github.com>
2019-01-04 10:35:19 -0800

    dsfg

--- CoolerFacebook/CoolerFacebook/Models/Friend.cs
+++ CoolerFacebook/CoolerFacebook/Models/Friend.cs
@@ -12,6 +12,12 @@ namespace CoolerFacebook.Models
         public int FriendId { get; set; }
         public virtual Profile Friend1 { get; set; }
         public virtual Profile Friend2 { get; set; }
+        public Friend() { }
+        public Friend(Profile fr1, Profile fr2)
+        {
+            this.Friend1 = fr1;
+            this.Friend2 = fr2;
+        }
 
     }
 }
