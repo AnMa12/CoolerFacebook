e430461a6d2f8098130d6752e5b474d4ab4139d2
laviniamihalachi <34279768+laviniamihalachi@users.noreply.github.com>
2019-01-05 15:02:37 -0800

    update groups

--- CoolerFacebook/CoolerFacebook/Controllers/GroupController.cs
+++ CoolerFacebook/CoolerFacebook/Controllers/GroupController.cs
@@ -51,14 +51,48 @@ namespace CoolerFacebook.Controllers
                         return View("Index");
                     }
             return RedirectToAction("Index");
-        }        public ActionResult Show(string GroupId)
+        }        public ActionResult Show(string GroupId, string text)
         {
             int id = int.Parse(GroupId);
             Group grp = db.Groups.Find(id);
             ViewBag.group = grp;
+            
+
+            if(text != null)
+            {
+                var currentUserId = User.Identity.GetUserId();
+                Profile currentProfile = db.Profiles.Where(i => i.User.Id == currentUserId).FirstOrDefault();
+                
+                Mesaj msj = new Mesaj(text, grp, currentProfile);
+                try
+                {
+                    db.Mesajs.Add(msj);
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    return View("Index");
+                }
+
+            }
 
             return View();
 
-        }
+        }        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+
+            Group grp = db.Groups.Find(id);
+
+            var currentUserId = User.Identity.GetUserId();
+            Profile currentProfile = db.Profiles.Where(i => i.User.Id == currentUserId).FirstOrDefault();
+
+            ProfileGroup pg = (from profg in db.ProfileGroups
+                               where profg.Profile.ProfileId == currentProfile.ProfileId && profg.Group.GroupId == grp.GroupId
+                               select profg).FirstOrDefault();
+            db.ProfileGroups.Remove(pg);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
--- CoolerFacebook/CoolerFacebook/Models/Mesaj.cs
+++ CoolerFacebook/CoolerFacebook/Models/Mesaj.cs
@@ -13,5 +13,13 @@ namespace CoolerFacebook.Models
         public string text { get; set; }
         public virtual Group Group { get; set; }
         public virtual Profile Profile { get; set; }
+
+        public Mesaj() { }
+        public Mesaj(string text, Group group, Profile profile)
+        {
+            this.text = text;
+            this.Group = group;
+            this.Profile = profile;
+        }
     }
 }
